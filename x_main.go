973bfeede852837d41fbf8ffb5a924274b0e6d18
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-03-02 02:06:53 -0800

    init

--- x_main.go
+++ x_main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"dist_kv"
 	"flag"
 )
 
 func main() {
-	config := dist_kv.loadConfig()
+	config := loadConfig()
 
 	var gossipPort, httpPort int
 	flag.IntVar(&gossipPort, "gossip", config.InternalPort, "port number for gossip protocol")
 	flag.IntVar(&httpPort, "http", config.ExternalPort, "port number for http server")
 	flag.Parse()
 
-	config.Host = dist_kv.GetLocalIP()
+	config.Host = GetLocalIP()
 	config.InternalPort = gossipPort
 	config.ExternalPort = httpPort
 
-	kv := dist_kv.NewDistKV(config)
+	kv := NewDistKV(config)
 	kv.Bootstrap()
 }
